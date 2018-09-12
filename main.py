ce31a88f53f62601986e798fba69a603faf4d23e
Maddie Wagner <musomaddy@gmail.com>
2018-09-12 14:40:14 +1000

    Added first draft of threads home page'

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ app.secret_key = "super_secret31415926535"
 @app.route('/')
 def main_page():
     return render_template('main_page.html')
+
+
+@app.route('/threads', methods=['GET', 'POST'])
+def thread_home():
+    return render_template('threads_home.html')
