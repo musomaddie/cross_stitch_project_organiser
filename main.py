7ba3e772c3c833a68bffa28cf85100260cfcf71e
Maddie Wagner <musomaddy@gmail.com>
2019-08-05 19:41:28 +1000

    Added very (very) basic main page

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect, url_for, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = "super_secret31415926535"
+app.secret = "super_secret11111"
+
+
+@app.route('/')
+def main_page():
+    return render_template('main_page.html')
