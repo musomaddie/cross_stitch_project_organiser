dfd702f73bc23b493fd2502b6324e87953a91a99
Maddie Wagner <musomaddy@gmail.com>
2019-08-06 11:08:24 +1000

    Added very simple thread page

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ app.secret = "super_secret11111"
 @app.route('/')
 def main_page():
     return render_template('main_page.html')
+
+
+@app.route('/threads')
+def threads_main():
+    return render_template("threads_main.html")
